2f6893a91f03a185d896243270359dc1519d266f
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-05-15 16:44:07 -0400

    Use mio event polling

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,25 @@
 #![feature(try_trait)]
+#![feature(duration_constants)]
 
 use std::{
     fs::File,
     io,
     io::{BufReader, Read},
-    net::{TcpListener, TcpStream},
-    os::unix::net::UnixStream,
+    net::SocketAddr,
     path::PathBuf,
-    sync::Arc,
+    sync::{mpsc, mpsc::TryRecvError, Arc},
     thread,
+    time::Duration,
 };
 
 use common::{Network, UpdateMessage};
 use dashmap::DashMap;
+use mio::{
+    net::{TcpListener, TcpStream},
+    Events, Interest, Poll, Token,
+};
+use signal_hook::iterator::Signals;
+use slab::Slab;
 use structopt::StructOpt;
 
 mod database;
@@ -24,6 +31,9 @@ const ACCEPTED_PROTO_VERSION: u8 = 66;
 struct Options {
     #[structopt(short, long, default_value = "./database", parse(from_os_str))]
     db_file: PathBuf,
+
+    #[structopt(short, long, default_value = "0.0.0.0:1515")]
+    address: SocketAddr,
 }
 
 fn main() -> io::Result<()> {
@@ -45,23 +55,64 @@ fn main() -> io::Result<()> {
         }
     };
     let db = Arc::new(Database::new(base));
-    let worked_db = db.clone();
-    thread::spawn(move || {
-        println!("Binding to localhost:1515");
-        let listener = TcpListener::bind("0.0.0.0:1515").unwrap();
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            let db_handle = worked_db.clone();
-            thread::spawn(move || handle(stream, db_handle));
+    // event polling setup
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(1024);
+    // set up signal handling
+    let mut signals = Signals::new(&[signal_hook::SIGINT])?;
+    const SIGNAL: Token = Token(0);
+    poll.registry()
+        .register(&mut signals, SIGNAL, Interest::READABLE)?;
+    // set up TCP
+    let mut listener = TcpListener::bind(opts.address)?;
+    const SERVER: Token = Token(1);
+    poll.registry()
+        .register(&mut listener, SERVER, Interest::READABLE)?;
+    // create slab
+    let mut slab = Slab::new();
+    // main event loop
+    'main: loop {
+        poll.poll(&mut events, Some(Duration::MILLISECOND))?;
+        let reap_slab = events.is_empty();
+        for event in &events {
+            match event.token() {
+                SERVER => loop {
+                    match listener.accept() {
+                        Ok((conn, _addr)) => {
+                            let clone = db.clone();
+                            let (tx, rx) = mpsc::channel::<()>();
+                            let handle = thread::spawn(move || -> io::Result<()> {
+                                handle(conn, clone)?;
+                                std::mem::drop(tx);
+                                Ok(())
+                            });
+                            slab.insert((rx, handle));
+                        }
+                        Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => break,
+                        Err(e) => return Err(e),
+                    }
+                },
+                SIGNAL => {
+                    println!("Got SIGINT, dying politely...");
+                    slab.shrink_to_fit();
+                    for (_, handle) in slab.drain() {
+                        // lol
+                        handle.join().unwrap()?;
+                    }
+                    break 'main;
+                }
+                Token(_) => {}
+            }
         }
-    });
-    println!("creating SIGINT handler...");
-    let (mut read, write) = UnixStream::pair()?;
-    signal_hook::pipe::register(signal_hook::SIGINT, write)?;
-    let mut buf = [0];
-    read.read_exact(&mut buf)?;
-    println!("happy shutdown!");
-    println!("the database has {} entries", db.records().len());
+        if reap_slab {
+            // Reap space from slab
+            slab.retain(|_key, val| match val.0.try_recv() {
+                Ok(_) => unreachable!(),
+                Err(_e @ TryRecvError::Disconnected) => false,
+                Err(_e @ TryRecvError::Empty) => true,
+            });
+        }
+    }
     Ok(())
 }
 
